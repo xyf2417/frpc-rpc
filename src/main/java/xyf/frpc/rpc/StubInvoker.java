84b6d2ba2332aa5ae5f3cf39931c4c3ae28d61b3
Yonfeng Xia <1206908474@qq.com>
2018-07-28 22:41:11 +0800

    stub

--- src/main/java/xyf/frpc/rpc/StubInvoker.java
+++ src/main/java/xyf/frpc/rpc/StubInvoker.java
@@ -6,26 +6,30 @@ public class StubInvoker <T> extends AbstractInvoker<T> {
 
 	private Invoker invoker;
 	
-	public StubInvoker(Invoker invoker) {
+	public Invoker getInvoker() {
+		return invoker;
+	}
+
+	public void setInvoker(Invoker invoker) {
 		this.invoker = invoker;
 	}
 
+	private Class stubClass;
+	
+	public StubInvoker(Invoker invoker,Class stubClass) {
+		this.invoker = invoker;
+		this.setStubClass(stubClass);
+	}
+
 	public Result invoke(Invocation invocation) throws RpcException {
-		try{
-			return invoker.invoke(invocation);
-		} catch(Exception e) {
-			RpcResult result = new RpcResult();
-			try{
-				Class clazz = this.getInterface();
-				Method method= clazz.getDeclaredMethod(invocation.getMethodName(), invocation.getParameterTypes());
-				Object returnValue = method.invoke(this.getProxy(), invocation.getArguments());
-				result.setStatus(ResultStatus.MOCK);
-				result.setValue(returnValue);
-			} catch(Exception em) {
-				result.setStatus(ResultStatus.MOCK_ERROR);
-				result.setValue(em.getMessage());
-			}
-			return result;
-		}
+		return null;
+	}
+
+	public Class getStubClass() {
+		return stubClass;
+	}
+
+	public void setStubClass(Class stubClass) {
+		this.stubClass = stubClass;
 	}
 }
--- src/main/java/xyf/frpc/rpc/proxy/StubProxyFactoryWrapper.java
+++ src/main/java/xyf/frpc/rpc/proxy/StubProxyFactoryWrapper.java
@@ -1,11 +1,74 @@
 package xyf.frpc.rpc.proxy;
 
-public class StubProxyFactoryWrapper implements ProxyFactory {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
+
+
+
+import xyf.frpc.rpc.StubInvoker;
+
+public class StubProxyFactoryWrapper implements ProxyFactory {
+	
+	private ProxyFactory proxyFactory;
+	
+	public StubProxyFactoryWrapper(ProxyFactory proxyFactory) {
+		this.proxyFactory = proxyFactory;
+	}
+
+	
 	public Object getProxy(Class interfaceClass, Object target,
 			boolean isProvider) {
-		// TODO Auto-generated method stub
-		return null;
+		if(isProvider) {
+			throw new IllegalStateException("Service can't use stub");
+		}
+		StubInvoker stubInvoker = (StubInvoker)target;
+		
+		Class stubClass = stubInvoker.getStubClass();
+		
+		Object proxied = proxyFactory.getProxy(interfaceClass, stubInvoker.getInvoker(), isProvider);
+		
+		Constructor<?> constructor;
+		Object proxy = null;
+		try {
+			constructor = findConstructor(stubClass, interfaceClass);
+			proxy = constructor.newInstance(new Object[]{proxied});
+		} catch (NoSuchMethodException e) {
+			throw new IllegalStateException("Service can't use stub");
+		} catch (InstantiationException e) {
+			throw new IllegalStateException("Instantiation stub exception");
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException("IllegalAccess stub exception");
+		} catch (IllegalArgumentException e) {
+			throw new IllegalStateException("IllegalArgument stub exception");
+		} catch (InvocationTargetException e) {
+			throw new IllegalStateException("InvocationTarget stub exception");
+		}
+
+		return proxy;
 	}
+	
+	 private static Constructor<?> findConstructor(Class<?> clazz, Class<?> paramType) throws NoSuchMethodException {
+	        Constructor<?> targetConstructor;
+	        try {
+	            targetConstructor = clazz.getConstructor(new Class<?>[]{paramType});
+	        } catch (NoSuchMethodException e) {
+	            targetConstructor = null;
+	            Constructor<?>[] constructors = clazz.getConstructors();
+	            for (Constructor<?> constructor : constructors) {
+	                if (Modifier.isPublic(constructor.getModifiers())
+	                        && constructor.getParameterTypes().length == 1
+	                        && constructor.getParameterTypes()[0].isAssignableFrom(paramType)) {
+	                    targetConstructor = constructor;
+	                    break;
+	                }
+	            }
+	            if (targetConstructor == null) {
+	                throw e;
+	            }
+	        }
+	        return targetConstructor;
+	    }
 
 }
